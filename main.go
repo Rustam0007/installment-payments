552dbb5ad9c597f282b0dc971dfc18c1868d8af1
Rustam0007 <rustamjonsaidov8@gmail.com>
2023-04-06 05:26:52 +0500

    refactor code. Add package internal

--- main.go
+++ main.go
@@ -3,45 +3,44 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	"installment-payments/internal"
 )
 
 func main() {
 
-	req1 := Request{
+	req1 := internal.Request{
 		ProductName: "phone",
 		Amount:      1000,
 		Phone:       "+992919010101",
 		Period:      18,
 	}
 
-	req2 := Request{
-		ProductName: "computer",
-		Amount:      1000,
-		Phone:       "+992919010101",
-		Period:      24,
-	}
+	//req2 := Request{
+	//	ProductName: "computer",
+	//	Amount:      1000,
+	//	Phone:       "+992919010101",
+	//	Period:      24,
+	//}
+	//
+	//req3 := Request{
+	//	ProductName: "tv",
+	//	Amount:      1000,
+	//	Phone:       "+992919010101",
+	//	Period:      24,
+	//}
 
-	req3 := Request{
-		ProductName: "tv",
-		Amount:      1000,
-		Phone:       "+992919010101",
-		Period:      24,
-	}
-
-	response1, err := InstallmentPayments(req1)
+	response1, err := internal.InstallmentPayments(req1)
 	if err != nil {
-		log.Println(err)
+		fmt.Errorf("[ERROR] - %v", err)
 	}
-	response2, err := InstallmentPayments(req2)
-	response3, err := InstallmentPayments(req3)
+	//response2, err := InstallmentPayments(req2)
+	//response3, err := InstallmentPayments(req3)
 
-	if err != nil {
-		log.Println("Error - ", err)
-	}
 	out, _ := json.MarshalIndent(response1, "", "  ")
-	out2, _ := json.MarshalIndent(response2, "", "  ")
-	out3, _ := json.MarshalIndent(response3, "", "  ")
+	//out2, _ := json.MarshalIndent(response2, "", "  ")
+	//out3, _ := json.MarshalIndent(response3, "", "  ")
 
-	fmt.Println(string(out), string(out2), string(out3))
+	fmt.Println(string(out))
+
+	//fmt.Println(string(out), string(out2), string(out3))
 }
